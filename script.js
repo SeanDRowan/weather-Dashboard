c830594c63ed76007dc57fcf3c076f55ce925b31
Sean <sdrowan93@gmail.com>
2023-07-07 17:44:34 -0500

    recieveing correct data from api

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const weatherBox =document.getElementById('weather-box')
 const cardContainer =document.getElementById('cardContainer')
 const citySearch =document.getElementById('citySearch')
 const searchButton =document.getElementById('searchButton')
-let lat = 0
-let lon = 0
+const cityText =document.getElementById('citytext')
 
 function nameToLatLon (){
     var cityName = 'SanAntonio'
@@ -14,24 +13,34 @@ function nameToLatLon (){
     })
     .then(function (data) {
         for (var i= 0; i < data.length; i++){
-        lat= (data[i].lat)
-        lon = (data[i].lon)    
+         lat= (data[i].lat);
+         lon = (data[i].lon);  
+         getApi() 
     }}); 
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&appid=749f3025aff34829991e7168b6bd4f9a'
+    
+}
+
+function getApi(){
+    requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&appid=749f3025aff34829991e7168b6bd4f9a'
     fetch(requestUrl)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            console.log(data)
+            for ( var i = 0;i<data.length;i++ ) {
+               var cityName = 
+                console.log(cityName)
+            }
     });
+
 }
 
-function getApi(){
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&appid=749f3025aff34829991e7168b6bd4f9a'
+
+
 
     
-}
+
 function parseApi(){
 
 }
