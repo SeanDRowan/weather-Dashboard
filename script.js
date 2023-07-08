d80ad3340a1f6157a7c57af9c1e3f37723f03240
Sean <sdrowan93@gmail.com>
2023-07-08 16:04:41 -0500

    parseing data

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const citySearch =document.getElementById('citySearch')
 const searchButton =document.getElementById('searchButton')
 const cityText =document.getElementById('citytext')
 
+
 function nameToLatLon (){
     var cityName = 'SanAntonio'
     var request = 'http://api.openweathermap.org/geo/1.0/direct?q='+cityName+ '&appid=749f3025aff34829991e7168b6bd4f9a'
@@ -15,6 +16,7 @@ function nameToLatLon (){
         for (var i= 0; i < data.length; i++){
          lat= (data[i].lat);
          lon = (data[i].lon);  
+         console.log(data)
          getApi() 
     }}); 
     
@@ -28,21 +30,34 @@ function getApi(){
         })
         .then(function (data) {
             console.log(data)
-            for ( var i = 0;i<data.length;i++ ) {
-               var cityName = 
-                console.log(cityName)
-            }
+            parseApi(data)
+          
     });
-
+    
 }
-
 
 
 
     
 
-function parseApi(){
+function parseApi(data){
+    for ( var i = 0;i<data.list.length;i++ ) {
+        city =data.city.name
+       cityText.textContent= city
 
+       time = data.list[i].dt
+       wind = data.list[i].wind.speed
+       temp = data.list[i].main.temp
+       humid = data.list[i].main.humidity
+       weather = data.list[i].weather
+       var d = new Date(time*1000);
+       dateStamp = d.getDate() + '/' + (d.getMonth()) +  " " + d.getFullYear() + d.getHours();
+        
+      console.log(dateStamp)
+      console.log(wind)
+      console.log(temp)
+      console.log(humid)
+        }
 }
 function assembleData(){
 
